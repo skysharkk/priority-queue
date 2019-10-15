b2f5c7f9e4c4eef5a8f0ef31de2a7b9d113f1e0c
skysharkk <sky.light.blr@gmail.com>
2019-10-15 10:16:01 +0300

    feat: done queue tests

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,35 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.maxSize = maxSize || 30;
+		this.heap = new MaxHeap();
+		this.counterSize = 0;
 	}
 
 	push(data, priority) {
-
+		if (this.heap.size() === this.maxSize) {
+			throw new Error('queue has max size');
+		} else {
+			this.heap.push(data, priority);
+			this.counterSize++;
+		}
 	}
 
 	shift() {
-
+		if (this.isEmpty()) {
+			throw new Error('queue is empty')
+		} else {
+			this.counterSize--;
+			return this.heap.pop();
+		}
 	}
 
 	size() {
-
+		return this.counterSize;
 	}
 
 	isEmpty() {
-		
+		return this.counterSize === 0;
 	}
 }
 
